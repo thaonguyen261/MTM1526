edebd5e1d72940921ee739bf6c6eb268de97d916
thaonguyen261 <105218140+thaonguyen261@users.noreply.github.com>
2022-05-16 19:54:43 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -33,14 +33,4 @@ result += `(!a || !c): ${!a || !c}\n`;
 
 alert(result);
 
-//Task 2
-// Assign variables 
-let first_name = 'Thao';
-let last_name = 'Nguyen Hoang';
-let email = 'nguy0986@algonquinlive.com'; 
-let output = 'My name is ' + first_name + ' ' + last_name + ' .You can contact me at ' + email +'.';
 
-
-// Alert output
-
-alert(output);
